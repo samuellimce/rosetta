44af1b139fcfb6451c61688f951991cb9562ade8
Samuel Lim <lim@ku.edu>
2024-01-30 15:59:26 -0600

    Add test cases to unit test

--- source/test/protocols/bootcamp/FoldTreeFromSSTests.cxxtest.hh
+++ source/test/protocols/bootcamp/FoldTreeFromSSTests.cxxtest.hh
@@ -32,6 +32,39 @@
 #include <core/pack/dunbrack/DunbrackRotamer.hh>
 
 
+utility::vector1< std::pair< core::Size, core::Size > >
+identify_secondary_structure_spans( std::string const & ss_string )
+{
+  utility::vector1< std::pair< core::Size, core::Size > > ss_boundaries;
+  core::Size strand_start = -1;
+  for ( core::Size ii = 0; ii < ss_string.size(); ++ii ) {
+    if ( ss_string[ ii ] == 'E' || ss_string[ ii ] == 'H'  ) {
+      if ( int( strand_start ) == -1 ) {
+        strand_start = ii;
+      } else if ( ss_string[ii] != ss_string[strand_start] ) {
+        ss_boundaries.push_back( std::make_pair( strand_start+1, ii ) );
+        strand_start = ii;
+      }
+    } else {
+      if ( int( strand_start ) != -1 ) {
+        ss_boundaries.push_back( std::make_pair( strand_start+1, ii ) );
+        strand_start = -1;
+      }
+    }
+  }
+  if ( int( strand_start ) != -1 ) {
+    // last residue was part of a ss-eleemnt                                                                                                                                
+    ss_boundaries.push_back( std::make_pair( strand_start+1, ss_string.size() ));
+  }
+  for ( core::Size ii = 1; ii <= ss_boundaries.size(); ++ii ) {
+    std::cout << "SS Element " << ii << " from residue "
+      << ss_boundaries[ ii ].first << " to "
+      << ss_boundaries[ ii ].second << std::endl;
+  }
+  return ss_boundaries;
+}
+
+
 // --------------- Test Class --------------- //
 
 class FoldTreeFromSSTests : public CxxTest::TestSuite {
@@ -56,6 +89,26 @@ public:
 		TS_ASSERT( true );
 	}
 
+	void test_ignore_characters() {
+		auto results = identify_secondary_structure_spans("   EEEEE   HHHHHHHH  EEEEE   IGNOR EEEEEE   HHHHHHHHHHH  EEEEE  HHHH   ");
+		utility::vector1< std::pair< core::Size, core::Size > >
+ expected = {{4, 8}, {12, 19}, {22, 26}, {36, 41}, {45, 55}, {58, 62}, {65, 68}};
+		TS_ASSERT_EQUALS(results, expected);
+	}
+
+	void test_neighbours() {
+		auto results = identify_secondary_structure_spans("HHHHHHH   HHHHHHHHHHHH      HHHHHHHHHHHHEEEEEEEEEEHHHHHHH EEEEHHH ");
+		utility::vector1< std::pair< core::Size, core::Size > >
+ expected = {{1, 7}, {11, 22}, {29, 40}, {41, 50}, {51, 57}, {59, 62}, {63, 65}};
+		TS_ASSERT_EQUALS(results, expected);
+	}
+
+	void test_single_characters() {
+		auto results = identify_secondary_structure_spans("EEEEEEEEE EEEEEEEE EEEEEEEEE H EEEEE H H H EEEEEEEE");
+		utility::vector1< std::pair< core::Size, core::Size > >
+ expected = {{1, 9}, {11, 18}, {20, 28}, {30, 30}, {32, 36}, {38, 38}, {40, 40}, {42, 42}, {44, 51}};
+		TS_ASSERT_EQUALS(results, expected);
+	}
 
 	// Shared finalization goes here.
 	void tearDown() {
