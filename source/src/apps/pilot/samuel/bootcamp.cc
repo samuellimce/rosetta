085f2364434259431a708b6a925d8ba85bb10471
Samuel Lim <lim@ku.edu>
2024-01-29 21:23:25 -0600

    Add simple Monte Carlo implementation

--- source/src/apps/pilot/samuel/bootcamp.cc
+++ source/src/apps/pilot/samuel/bootcamp.cc
@@ -17,6 +17,7 @@
 #include <core/scoring/ScoreFunction.hh>
 #include <core/scoring/ScoreFunctionFactory.hh>
 #include <numeric/random/random.functions.hh>
+#include <protocols/moves/MonteCarlo.hh>
 
 int main( int argc, char ** argv ) {
         // Parse arguments
@@ -30,6 +31,43 @@ int main( int argc, char ** argv ) {
                 std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
                 // Use the file.
                 core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
+                // Produce a score from the scoring function
+                auto sfxn = core::scoring::get_score_function();
+                core::Real score = sfxn->score( *mypose );
+                std::cout << "The score is " << score << std::endl;
+                // Monte Carlo starts here.
+                auto mc = protocols::moves::MonteCarlo(*mypose, *sfxn, numeric::random::uniform());
+                auto probability = numeric::random::gaussian();
+                auto uniform_random_number = numeric::random::uniform();
+                
+                for (core::Size i = 0; i < 1000; i++) {
+                        // Initialize random values.
+                        core::Size rand_res = uniform_random_number * (mypose->size() / mypose->total_residue()) + 1;
+                        core::Real pert1 = numeric::random::uniform() * 360 - 180;
+                        core::Real pert2 = numeric::random::uniform() * 360 - 180;
+
+                        std::cout << pert1 << " " << pert2 << std::endl;
+                        // Store angles.
+                        core::Real orig_phi = mypose->phi( rand_res );
+                        core::Real orig_psi = mypose->psi( rand_res );
+
+                        std::cout << orig_phi << " " << orig_psi << std::endl;
+                        // Update angles.
+                        mypose->set_phi( rand_res, orig_phi + pert1 );
+                        mypose->set_psi( rand_res, orig_psi + pert2 );
+
+                        std::cout << mypose->phi( rand_res ) << " " << mypose->psi( rand_res ) << std::endl;
+                        // Metropolis.
+                        if (mc.boltzmann(*mypose)) {
+                                std::cout << "Pose accepted." << std::endl;
+                        } else {
+                                std::cout << "Pose rejected." << std::endl;
+                        }
+
+                        std::cout << mypose->phi( rand_res ) << " " << mypose->psi( rand_res ) << std::endl;
+
+                }
+                
         } else {
                 std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
                 return 1;
