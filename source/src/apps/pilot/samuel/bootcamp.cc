5c047307cd267fd5d17178e5ed8bf2063c5300ba
Samuel Lim <lim@ku.edu>
2024-01-29 20:50:09 -0600

    Implement pose import

--- source/src/apps/pilot/samuel/bootcamp.cc
+++ source/src/apps/pilot/samuel/bootcamp.cc
@@ -8,9 +8,33 @@
 // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
 #include <iostream>
+#include <basic/options/option.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <devel/init.hh>
+#include <core/import_pose/import_pose.hh>
+#include <utility/pointer/owning_ptr.hh>
+#include <core/pose/Pose.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
+#include <numeric/random/random.functions.hh>
 
-int main() {
-        std::cout << "Hello World!" << std::endl;
+int main( int argc, char ** argv ) {
+        // Parse arguments
+        devel::init( argc, argv );
+        // Get filenames
+        utility::vector1< std::string > filenames = basic::options::option[
+        basic::options::OptionKeys::in::file::s ].value();
+        // If a file is passed...
+        if ( filenames.size() > 0 ) {
+                // Print the file.
+                std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
+                // Use the file.
+                core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
+        } else {
+                std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
+                return 1;
+        }
+
         return 0;
 }
 
